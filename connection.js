f746f08b84670a279981a3eeb327af8e68cdcef2
subash-v <subash.v@xelpmoc.in>
2022-12-21 12:09:47 +0530

    chnages

--- connection.js
+++ connection.js
@@ -3,10 +3,11 @@ require('dotenv').config();
 let mysql = require('mysql');
 
 let con = mysql.createPool({
-    connectionLimit : 5,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    user: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD
+  connectionLimit: 5,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 module.exports = con
